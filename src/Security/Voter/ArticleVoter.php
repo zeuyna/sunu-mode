58d372e354e522b5d967e4c62260b63e618f02f4
seynabou diop <diopnabou08@gmail.com>
2020-06-03 14:57:44 +0100

    ArticleVoter

--- src/Security/Voter/ArticleVoter.php
+++ src/Security/Voter/ArticleVoter.php
@@ -1,11 +1,10 @@
 
 <?php
 
-namespace App\Security\Voter\ArticleVoter;
+namespace App\Security\Voter;
 
 use App\Entity\Article;
 
-use App\Entity\User;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 use Symfony\Component\Security\Core\User\UserInterface;
